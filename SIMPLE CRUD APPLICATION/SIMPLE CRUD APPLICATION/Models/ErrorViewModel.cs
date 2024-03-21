e1379d12eee137736e936ed8ac6c2585ce30a007
unknown <mujtubasteam0333@gmail.com>
2024-03-21 23:42:06 +0500

    Complete Project Along with Database Backup

---
+++ SIMPLE CRUD APPLICATION/SIMPLE CRUD APPLICATION/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIMPLE_CRUD_APPLICATION.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SIMPLE CRUD APPLICATION/SIMPLE CRUD APPLICATION/Models/PosContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SIMPLE_CRUD_APPLICATION.Models;
+
+public partial class PosContext : DbContext
+{
+    public PosContext()
+    {
+    }
+
+    public PosContext(DbContextOptions<PosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-QHSL28H\\SQLEXPRESS; Database=POS; Trusted_Connection=True; trustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC07D70647EE");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Category)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Description)
+                .HasMaxLength(200)
+                .IsFixedLength();
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Price).HasColumnType("money");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
